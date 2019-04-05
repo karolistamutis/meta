00f15d46dc7a668f1fa2ae8fa66813336f510d4c
Karolis Tamutis <karolis.t@gmail.com>
2019-04-05 10:15:41 +0300

    We're actually working with ansible

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,11 @@
 
 set -euo pipefail
 
-VAGRANT_USER="vagrant"
-VAGRANT_GROUP="vagrant"
+# configurable params
+ANSIBLE_USER="ansible"
+ANSIBLE_GROUP="ansible"
+ANSIBLE_PUBKEY_URI="https://raw.githubusercontent.com/karolistamutis/meta/master/pubkeys/ansible.pub"
+SSHD_CONFIG_URI="https://raw.githubusercontent.com/karolistamutis/meta/master/configs/sshd_config"
 
 err_report() {
   echo "Exited with error on line $1"
@@ -42,41 +45,52 @@ ssh_exec () {
 }
 
 #
-# Setup Vagrant user
+# Setup Ansible user
 #
 
 # add goup
-cmd="grep -c $VAGRANT_GROUP /etc/group"
+cmd="grep -c $ANSIBLE_GROUP /etc/group"
 
 # https://stackoverflow.com/questions/41078968/why-is-grep-c-with-0-count-exits-program-with-status-code-1
 out=$(ssh_exec $cmd) || :
 
 if [ "$out" == "0" ]; then
-  $(ssh_exec "groupadd $VAGRANT_GROUP")
-  echo "group $VAGRANT_GROUP added"
+  $(ssh_exec "groupadd $ANSIBLE_GROUP")
+  echo "group $ANSIBLE_GROUP added"
 else
-  echo "group $VAGRANT_GROUP exists, skipping"
+  echo "group $ANSIBLE_GROUP exists, skipping"
 fi
 
 # add user
-cmd="grep -c $VAGRANT_USER /etc/passwd"
+cmd="grep -c $ANSIBLE_USER /etc/passwd"
 out=$(ssh_exec $cmd) || :
 
 if [ "$out" == "0" ]; then
-  out=$(ssh_exec "useradd -m -g $VAGRANT_GROUP $VAGRANT_USER")
+  out=$(ssh_exec "useradd -m -g $ANSIBLE_GROUP $ANSIBLE_USER")
   echo "user $VAGRANT_USER added"
 else
   echo "user $VAGRANT_USER exists, skipping"
 fi
 
 # add to sudoers
-cmd="cat >/etc/sudoers.d/90-vagrant-users <<< '%$VAGRANT_GROUP   ALL=(ALL:ALL) ALL'; chmod 440 /etc/sudoers.d/90-vagrant-users"
+cmd="cat >/etc/sudoers.d/90-ansible-users <<< '%$ANSIBLE_GROUP   ALL=(ALL:ALL) ALL'; chmod 440 /etc/sudoers.d/90-ansible-users"
 out=$(ssh_exec $cmd)
-echo "added /etc/sudoers.d/90-vagrant-users"
+echo "added /etc/sudoers.d/90-ansible-users"
 
 # add ssh public key
-#cmd="curl -o- "
+cmd="mkdir -p /home/$ANSIBLE_USER/.ssh && curl $ANSIBLE_PUBKEY_URI > /home/$ANSIBLE_USER/.ssh/authorized_keys"
+out=$(ssh_exec $cmd)
+echo "added $ANSIBLE_USER public key to ssh auth"
 
 #
 # Harden sshd
 # 
+
+cmd="cp /etc/ssh/sshd_config /etc/ssh/sshd_config.backup && curl -so- $SSHD_CONFIG_URI|sed 's/ALLOW_USERS/$ANSIBLE_USER/'>/etc/ssh/sshd_config"
+out=$(ssh_exec $cmd)
+echo "updated /etc/ssh/sshd_config"
+
+# restart sshd
+cmd="systemctl restart ssh"
+out=$(ssh_exec $cmd)
+echo "restarted sshd"
